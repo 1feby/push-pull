e067a58d55149350d9d4dacae9a52a091c834397
1feby <feby.gergis97@eng-st.cu.edu.eg>
2021-04-18 03:13:35 +0200

    add socket lab

---
+++ socket/Lab2-backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+  const app = express();
+  const PORT = process.env.PORT || 8000;
+  const core = require('cors');
+  app.use(express.json());
+  app.use(core());
+
+  const http= require('http');
+  const server=http.createServer();
+  const io=require('socket.io')(server,{
+     cors:{
+         origin: '*',
+         method: '*',
+     },
+  });
+
+  io.on('connection',(socket)=>{
+     console.log('New Connection' , socket.id)
+     socket.on('message',(message)=>{
+         if(message.id){
+
+             io.to(message.id).emit('message',message)
+             console.log(message.id)
+         }
+         else{
+
+             socket.broadcast.emit('message',message);
+         }
+
+     });
+  });
+
+  server.listen(PORT, (err) => {
+     if (err) console.error( err);
+     console.info(`App server is running and listening on port ${PORT}`);
+
+  });
